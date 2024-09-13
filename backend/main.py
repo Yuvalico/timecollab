2b326788ae0190af51ca340dd67b76b41ee8c5f5
Yuval Safran <yuvalsaf@gmail.com>
2024-09-13 16:04:01 +0300

    changed base api url

--- backend/main.py
+++ backend/main.py
@@ -5,15 +5,17 @@ from endpoints.users import users_blueprint
 from config import Config
 from dotenv import load_dotenv
 
+BASE_API = "/api"
 load_dotenv()
+
 app = Flask(__name__)
 app.config.from_object(Config)
 # app.config['SECRET_KEY'] = 'your_jwt_secret_key'  # Replace this with a real secret key
 
 # Register the blueprints
 app.register_blueprint(auth_blueprint)
-app.register_blueprint(companies_blueprint, url_prefix='/companies')
-app.register_blueprint(users_blueprint, url_prefix='/users')
+app.register_blueprint(companies_blueprint, url_prefix=BASE_API+'/companies')
+app.register_blueprint(users_blueprint, url_prefix=BASE_API+'/users')
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
