4dd178e038842fdf0a98e997739c1d38f35e58d7
Yuval Safran <yuvalsaf@gmail.com>
2024-09-13 16:08:05 +0300

    auth path

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.config.from_object(Config)
 # app.config['SECRET_KEY'] = 'your_jwt_secret_key'  # Replace this with a real secret key
 
 # Register the blueprints
-app.register_blueprint(auth_blueprint)
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(companies_blueprint, url_prefix=BASE_API+'/companies')
 app.register_blueprint(users_blueprint, url_prefix=BASE_API+'/users')
 
